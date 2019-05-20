3e00d3e00eb088ce4a32fe9bf71088d71a8cf34c
Maciul <pawel.maciulewski@gmail.com>
2019-05-20 14:02:41 -0600

    add bin

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "strings"
   "fmt"
   "os"
+  "log"
 )
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
@@ -19,20 +20,22 @@ func sayNothing(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Nothing")
 }
 
-func getPort() string{
-    if ( os.Getenv("APP_ENV") == "production") {
-      return ":80"
-    } else {
-      return "127.0.0.1:8080"
-    }
-}
-
 func main() {
+  port := "8080"
+  
+  if (os.Getenv("PORT") != "" ) {
+    port = os.Getenv("PORT")
+  }
+
+  if port == "" {
+        log.Fatal("$PORT must be set")
+  }
+
   http.HandleFunc("/dont/", sayNothing)
   http.HandleFunc("/", sayHello) 
   http.Handle("/serve-static-files/", http.StripPrefix("/serve-static-files/", http.FileServer(http.Dir("static/"))))
   
-  if err := http.ListenAndServe( getPort(), nil); err != nil {
+  if err := http.ListenAndServe( ":" + port, nil); err != nil {
     panic(err)
   }
 }
